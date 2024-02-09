5e8f919549fe2c87607fd536467bea5b2580ded8
PericoMar <pedromartinez.22@campuscamara.es>
2024-02-09 15:57:40 +0100

    [Update] Minor fix

--- countryApp/src/app/app.component.ts
+++ countryApp/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
-import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,SideBarComponent, AboutPageComponent],
+  imports: [CommonModule, RouterOutlet,SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
